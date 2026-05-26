e129ccf92087f9554e832ab21be193803c216ce9
Elizarov Yaroslav <rosyoeliz@yandex.ru>
2026-05-26 12:26:47 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -55,9 +55,9 @@ dnf5 -y install xcb-util-cursor # fix amnezia vpn work
 
 # dnf5 -y install codium
 
-# rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-# dnf5 -y install code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf5 -y install code
 
 # dnf5 -y install curl cabextract xorg-x11-font-utils fontconfig
 
